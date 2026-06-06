6f64ebb8263a47ef557a629818c2b0f29e2e4d11
xcurx <suj5al78@gmail.com>
2026-06-06 14:31:26 +0530

    feat: initialize backend project and scaffold Next.js frontend with authentication models and Prisma integration

---
+++ frontend/prisma/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@/app/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { withAccelerate } from "@prisma/extension-accelerate"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({ adapter }).$extends(withAccelerate())
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
